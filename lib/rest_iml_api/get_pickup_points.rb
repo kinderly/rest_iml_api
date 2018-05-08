212350407089ff7ec16873966340f14811606b73
Andrey Stikheev <andrey.stikheev@gmail.com>
2018-05-08 19:49:42 +0300

    Забираем список ПВЗ из более правильного места

--- lib/rest_iml_api/get_pickup_points.rb
+++ lib/rest_iml_api/get_pickup_points.rb
@@ -1,11 +1,10 @@
 module RestImlApi
   class GetPickupPointsList
-    URL = 'http://api.iml.ru/list/sd'.freeze
+    URL = 'http://list.iml.ru/sd'.freeze
     class << self
       def call
         uri = URI(URL)
         req = Net::HTTP::Get.new(uri)
-        req.basic_auth(ENV['IML_LOGIN'], ENV['IML_PASSWORD'])
         http = Net::HTTP.new(uri.host, uri.port)
         res = http.request(req)
         res.is_a?(Net::HTTPOK) ? JSON.parse(res.body) : res
